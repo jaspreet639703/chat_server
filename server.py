f579582fb7aa9c60daaabcd2855e0e0ce29881c1
jaspreet639703 <jaspreet639703@gmail.com>
2026-03-17 23:20:45 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,117 +1,166 @@
 import socket
 import threading
+import os
 from datetime import datetime
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 Conntd_clients = {}
 lock = threading.Lock()
 
-#server log
+
+# ---------------- HTTP HEALTH SERVER ----------------
+class HealthHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Chat server running")
+
+
+def start_http_server(port):
+    httpd = HTTPServer(("0.0.0.0", port), HealthHandler)
+    httpd.serve_forever()
+
+
+# ---------------- SERVER LOG ----------------
 def log_server(event):
     with open("server_log.txt", "a", encoding="utf-8") as f:
         time = datetime.now().strftime("%H:%M:%S")
         f.write("[" + time + "] " + event + "\n")
 
-#chat history
+
+# ---------------- CHAT LOG ----------------
 def log_chat(msg):
     with open("chat_history.txt", "a", encoding="utf-8") as f:
         time = datetime.now().strftime("%H:%M:%S")
         f.write("[" + time + "] " + msg + "\n")
 
+
+# ---------------- BROADCAST ----------------
 def broadcast_msg(msg, sender_sckt=None):
-    # Send message to all connected clients
-    for client_sckt in list(Conntd_clients):
+
+    with lock:
+        clients = list(Conntd_clients.keys())
+
+    for client_sckt in clients:
         if client_sckt != sender_sckt:
             try:
                 client_sckt.send(msg.encode("utf-8"))
             except:
+                with lock:
+                    if client_sckt in Conntd_clients:
+                        del Conntd_clients[client_sckt]
                 client_sckt.close()
-                if client_sckt in Conntd_clients:
-                    del Conntd_clients[client_sckt]
 
-# Handle each client
+
+# ---------------- CLIENT HANDLER ----------------
 def handle_client(client_sckt, adrs):
+
     username = None
-    print("Client Connected:-", adrs)
-    log_server("Client Connected:- " + str(adrs))
-    
+    print("Client Connected:", adrs)
+
     try:
         while True:
+
             msg = client_sckt.recv(1024).decode("utf-8")
+
             if not msg:
                 break
-            
-            parts = msg.split(" ", 1)  #divide msg from 1st space
+
+            parts = msg.split(" ", 1)
             cmd = parts[0]
-            
-            #JOIN cmd
+
             if cmd == "JOIN":
+
                 username = parts[1].strip()
-                
+
                 with lock:
                     if username in Conntd_clients.values():
-                        client_sckt.send("Username is already taken.".encode("utf-8"))
+                        client_sckt.send("Username taken".encode())
                         continue
+
                     Conntd_clients[client_sckt] = username
 
                 join_msg = "## " + username + " joined the chat ##"
                 print(join_msg)
-                log_server(join_msg)
                 broadcast_msg(join_msg)
-            
-            #MSG cmd
+
             elif cmd == "MSG":
-                if len(parts)>1:
-                    text = parts[1]
-                else:
-                    continue
+
+                text = parts[1]
                 time = datetime.now().strftime("%H:%M")
+
                 chat_msg = "[" + time + "] " + username + ": " + text
 
                 print(chat_msg)
                 log_chat(chat_msg)
+
                 broadcast_msg(chat_msg, client_sckt)
-            
-            #QUIT cmd
-            elif cmd == "QUIT":
-                break
-            
+
             elif cmd == "USERS":
+
                 with lock:
                     user_list = ", ".join(Conntd_clients.values())
-                All_Users = "All users: " + user_list
-                client_sckt.send(All_Users.encode("utf-8"))
-            
+
+                client_sckt.send(("All users: " + user_list).encode())
+
+            elif cmd == "QUIT":
+                break
+
     except:
         pass
-    
+
     finally:
+
         with lock:
             if client_sckt in Conntd_clients:
                 username = Conntd_clients[client_sckt]
                 del Conntd_clients[client_sckt]
-        
+
         client_sckt.close()
-        
+
         if username:
             leave_msg = "## " + username + " left the chat ##"
-            print(leave_msg)
-            log_server(leave_msg)
-            broadcast_msg(leave_msg)     
+            broadcast_msg(leave_msg)
 
-def start_server():
-    srvr_sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #Address Family use IPv4 and use TCP
-    
-    srvr_sckt.bind(("0.0.0.0", 8000))
-    
+
+# ---------------- TCP CHAT SERVER ----------------
+def start_chat_server():
+
+    tcp_port = 9000   # chat server port
+
+    srvr_sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    srvr_sckt.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    srvr_sckt.bind(("0.0.0.0", tcp_port))
     srvr_sckt.listen(10)
-    
-    print("Chat server started on the Port:-", 8000)
-    log_server("Chat server started on the Port:- 8000")
-    
+
+    print("Chat server started on port:", tcp_port)
+
     while True:
+
         client_sckt, adrs = srvr_sckt.accept()
-        thread = threading.Thread(target = handle_client, args=(client_sckt,adrs))
+
+        thread = threading.Thread(
+            target=handle_client,
+            args=(client_sckt, adrs),
+            daemon=True
+        )
+
         thread.start()
 
+
+# ---------------- MAIN ----------------
 if __name__ == "__main__":
-    start_server()
+
+    port = int(os.environ.get("PORT", 10000))
+
+    # start HTTP server (Render check)
+    threading.Thread(
+        target=start_http_server,
+        args=(port,),
+        daemon=True
+    ).start()
+
+    # start TCP chat server
+    start_chat_server()
